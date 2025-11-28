ac263bfde8115b2694119b3f48a48e89f86fd264
Daniel Kelly <daniel.kelly@thegoldsmiths.co.uk>
2025-11-28 16:13:48 +0000

    Action Description static Dim added from SSIS package to create unique primary key.  CTE added to fact table

--- models/staging/UserActions/stgActionFact.sql
+++ models/staging/UserActions/stgActionFact.sql
@@ -1,5 +1,11 @@
+{{ config(materilized='table')}}
+WITH ActionFact As (
+
 SELECT DISTINCT SiteKey, PACKETID AS PacketKey, DateKey, UserKey, ActionKey, TotalItems, TotalSampled
 FROM {{ ref('stgUnionActions') }}  INNER JOIN {{ source('DBT_SNOWFLAKE', 'SITEDIM') }} ON dbo.SITEDIM.SiteDescription = stgUnionActions.SITEDESC 
 INNER JOIN {{ source('DBT_SNOWFLAKE', 'USER_DIM') }}  ON User_Dim.UserName = stgUnionActions.UserName
 INNER JOIN {{ source('DBT_SNOWFLAKE', 'ACTIONDIM') }} ON ACTIONDIM.ActionDescription = stgUnionActions.ActionDesc
 INNER JOIN {{ ref('stgDateDim') }} ON stgDateDim.ActionDate = stgUnionActions.ActionDate 
+
+)
+SELECT * FROM ActionFact
