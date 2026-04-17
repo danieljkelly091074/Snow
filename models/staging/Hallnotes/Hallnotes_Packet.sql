bd4942ce7325a52fed6fcb6e1c0e60514fe3fc75
Daniel Kelly <daniel.kelly@thegoldsmiths.co.uk>
2026-04-17 15:38:24 +0000

    LLM changes

--- models/staging/Hallnotes/Hallnotes_Packet.sql
+++ models/staging/Hallnotes/Hallnotes_Packet.sql
@@ -107,4 +107,10 @@ enriched as (
         and apk.COUNTERDATE = f.RECEIVEDDATE
 )
 
-select * from enriched 
+select * from enriched e
+where not exists (
+    select 1
+    from {{ source('sharepoint','HALLNOTES_PACKETNUMBER') }} hp
+    where hp.PACKETNUMBER = e.PACKETNUMBER
+      and hp.RECEIVEDDATE = e.RECEIVEDDATE
+)
